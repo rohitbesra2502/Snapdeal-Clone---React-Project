9117af73d572c98816d1fb6b1a8a5a1d9d363d44
Rohit Besra <rohitkumarbesra786@gmail.com>
2023-07-31 18:22:36 +0530

    pushed all snapdeal codes

---
+++ src/components/ProductsImageSlider.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ImageSlider from './ImageSlider.jsx';
+
+const ProductsImageSlider = () => {
+    return (
+        <div className='image-slider'>
+            <div className='images'>
+                <ImageSlider/>
+            </div>
+        </div>
+    )
+}
+
+export default ProductsImageSlider;
